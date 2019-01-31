37cdc579692f357c3dc7628fdeeb48b89ff83733
Rex Brooks <rexb@starbourne.com>
2019-01-31 12:25:10 -0800

    Initial Adds

---
+++ edxl_cap_v1_2/Models/AlertModel.cs
@@ -0,0 +1,16 @@
+﻿using edxl_cap_v1_2.Models.ContentViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace edxl_cap_v1_2.Models
+{
+    class AlertModel : EdxlCapMessageViewModel
+    {
+        new int AlertIndex { get; set; }
+
+        new int SelectedAlertIndex { get; set; }
+
+    }
+}
---
+++ edxl_cap_v1_2/Models/ContentViewModels/EdxlCapMessageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using edxl_cap_v1_2.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace edxl_cap_v1_2.Models.ContentViewModels
+{
+    public class EdxlCapMessageViewModel
+    {
+        public EdxlCapMessageViewModel()
+        {
+        }
+
+        public EdxlCapMessageViewModel(string v, object p)
+        {
+        }
+
+        [Key]
+        public int AlertIndex { get; set; }
+        public string Alert_Identifier { get; set; }
+        public int SelectedAlertIndex { get; set; }
+        [NotMapped]
+        public List<SelectListItem> Alert_Identifiers { get; set; }
+        public List<AlertVm> Alerts { get; set; }
+
+        public Alert Alert { get; set; }
+
+        public Info Info { get; set; }
+
+        public Area Area { get; set; }
+
+        public Resource Resource { get; set; }
+    }
+
+
+
+}
