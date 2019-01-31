5128d9a857b2ef4fb48d1d48521a8519da347782
Rex Brooks <rexb@starbourne.com>
2019-01-31 12:28:13 -0800

    Initial Adds

---
+++ Services/GetSelectedAlertIndexService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace edxl_cap_v1_2.Services
+{
+    public class GetSelectedAlertIndexService
+    {
+        private readonly ILogger<GetSelectedAlertIndexService> _logger;
+
+        public GetSelectedAlertIndexService(ILogger<GetSelectedAlertIndexService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task GetSelectedAlertIndex(int AlertIndex)
+        {
+            _logger.LogInformation(
+                "GetSelectedAlertIndex.GetSelectedAlertIndex");
+
+            return Task.FromResult(0);
+        }
+    }
+}
