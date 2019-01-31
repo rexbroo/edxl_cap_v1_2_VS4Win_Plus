bf5a40a86ac36e6291cfb1d0b32cf20632b09224
Rex Brooks <rexb@starbourne.com>
2019-01-31 12:22:32 -0800

    Initial Adds

---
+++ Interfaces/IGetSelectedAlertIndex.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace edxl_cap_v1_2.Interfaces
+{
+    public interface IGetSelectedAlertIndex
+    {
+        Task GetSelectedAlertIndex(int AlertIndex);
+        Task GetSelectedAlertIndex();
+    }
+}
