cd1447025c8e7dbd57ab6534689db3df6eff6adb
Rex Brooks <rexb@starbourne.com>
2019-01-31 12:29:57 -0800

    Initial Adds

---
+++ edxl_cap_v1_2/ViewComponents/EdxlCapMsgViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using edxl_cap_v1_2.Models;
+using edxl_cap_v1_2.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace edxl_cap_v1_2.ViewComponents
+{
+    public class EdxlCapMsgViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+
+        public EdxlCapMsgViewComponent(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+        int SelectedAlertIndex, string Alert_Identifier)
+        {
+            var items = await GetItemsAsync(SelectedAlertIndex, Alert_Identifier);
+            return View(items);
+        }
+        private Task<List<EdxlCapMsg>> GetItemsAsync(int SelectedAlertIndex, string Alert_Identifier)
+        {
+            return db.EdxlCapMsg.Where(x => x.Alert_Identifier == Alert_Identifier &&
+                                 x.AlertIndex <= SelectedAlertIndex).ToListAsync();
+        }
+    }
+}
